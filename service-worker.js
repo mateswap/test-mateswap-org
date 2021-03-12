472ccc94ec9d26e475ce8f461454e2de613b0fef
MG <1121506496@qq.com>
2021-03-12 14:08:20 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bf6824475be704f139a9334535c998b0.js"
+  "./precache-manifest.9049809cc034f4f5768d2482df0bb9f3.js"
 );
 
 self.addEventListener('message', (event) => {
